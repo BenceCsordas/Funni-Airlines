fb47f65e65434a6c183f6893cb978429c4604f34
BenceCsordas <bencecs1@gmail.com>
2025-06-04 10:01:10 +0200

    CLI feladatok

---
+++ src/com/company/kando/FlightPassanger.java
@@ -0,0 +1,15 @@
+package com.company.kando;
+
+public class FlighPassanger {
+
+    public String FlightNumber;
+    public String Source;
+    public String Destination;
+    public String PassangerName;
+    public int PassangerId;
+    public String TicketId;
+    public String SeatNumber;
+    public int Price;
+
+
+}
